0803f4da9d2e663e74b0bc20b7a15bffcbd94033
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-21 00:06:26 +0100

    Styles active navbar using classNames package

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
   const links = [
     {
       label: "Dashboard",
@@ -10,7 +14,7 @@ const Navbar = () => {
     },
     {
       label: "Issues",
-      href: "issues",
+      href: "/issues",
     },
   ];
   return (
@@ -22,7 +26,11 @@ const Navbar = () => {
         <Link
           key={link.href}
           href={link.href}
-          className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          className={classNames({
+            "text-zinc-900": link.href === currentPath,
+            "hover:text-zinc-800 transition-colors": link.href !== currentPath,
+            "text-zinc-500": true,
+          })}
         >
           {link.label}
         </Link>
