3b51c3a04291dda8de9992cca7e451b883cbef36
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-20 23:27:00 +0100

    Created navbar basic version

---
+++ app/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+
+const Navbar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "issues",
+    },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 h-14 items-center pl-4">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className="text-zinc-500 hover:text-zinc-800 transition-colors"
+        >
+          {link.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
