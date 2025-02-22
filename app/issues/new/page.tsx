cfef28c6444f46ac1a41f84798e6a24268c32b95
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-22 18:37:32 +0100

    Added markdown editor, and implemented saving issues to be

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,36 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { Button, TextField } from "@radix-ui/themes";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import axios from "axios";
+interface IssueInput {
+  title: string;
+  description: string;
+}
 
 const IssueNewPage = () => {
+  const { register, handleSubmit, control } = useForm<IssueInput>();
+  const router = useRouter();
+  const onSubmit: SubmitHandler<IssueInput> = async (data) => {
+    await axios.post("/api/issues", data);
+    router.push("/issues");
+  };
+
   return (
-    <div className="space-y-3 max-w-xl">
-      <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Description" />
-      <Button>Submit New Issue</Button>
-    </div>
+    <form className="space-y-3 max-w-xl" onSubmit={handleSubmit(onSubmit)}>
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        control={control}
+        name="description"
+        rules={{ required: true }}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+      <Button type="submit">Submit New Issue</Button>
+    </form>
   );
 };
 
