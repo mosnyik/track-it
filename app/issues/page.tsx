1d509ab2f45d35566602fbf9746969a323dbcc6f
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-23 22:50:37 +0100

    Created a costome Link component for hybrid the Radix link and the Next link behaviors and functionalities

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import IssueStatus from "../components/IssueStatus";
 import delay from "delay";
 import NewIssueButton from "../components/NewIssueButton";
-import Link from "next/link";
+import Link from "../components/Link";
 
 const Issues = async () => {
   const issues = await prisma.issue.findMany();
