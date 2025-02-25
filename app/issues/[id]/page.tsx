66c9f2ab91ec5115fd5602b7b0aaef4acd1818e3
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-25 21:11:02 +0100

    Added a delet button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { prisma } from "@/prisma/prismaClient";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetailsData from "./IssueDetailsData";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -18,12 +19,15 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: "1", sm: "2" }} className="space-y-3">
-      <Box>
+    <Grid columns={{ initial: "1", sm: "5" }} className="space-y-3">
+      <Box className="col-span-4  ">
         <IssueDetailsData issue={issue} />
       </Box>
       <Box>
-        <EditIssueButton issueId={issue.id} />
+        <Flex direction="column" gap="4">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
       </Box>
     </Grid>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./theme-config.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "./Navbar";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,7 +36,9 @@ export default function RootLayout({
       >
         <Theme accentColor="teal">
           <Navbar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
