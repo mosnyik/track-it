cc081f2a11cb5b16e8be86f84cfc451c3bba2b42
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-25 23:30:34 +0100

    Errors handled

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,18 +1,22 @@
 "use client";
+import Spinner from "@/app/components/Spinner";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { setErrorMap } from "zod";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
   return (
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete Issue
+            {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content maxWidth="450px">
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
@@ -33,10 +37,13 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
                 color="red"
                 onClick={async () => {
                   try {
+                    setIsDeleting(true);
                     await axios.delete("/api/issues/" + issueId);
                     router.push("/issues");
                     router.refresh();
                   } catch (err) {
+                    console.log(err)
+                    setIsDeleting(true);
                     setError(true);
                   }
                 }}
