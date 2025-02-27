e294f21b1cb8b88e2771654ab985a60785365e73
Nyikwagh Moses <mosnyik@gmail.com>
2025-02-27 16:53:15 +0100

    Built API for assigning issue and populate the AssignSeletor comp

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,32 @@
+"use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+      return users;
+    }
+
+    fetchUsers();
+  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assignee..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="orange">Orange</Select.Item>
-          <Select.Item value="apple">Apple</Select.Item>
-          <Select.Item value="grape">Grape</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
