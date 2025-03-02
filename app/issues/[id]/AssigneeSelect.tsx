36e5b3e56fb1e17e4b5376fa3b399d91a963c255
Nyikwagh Moses <mosnyik@gmail.com>
2025-03-02 16:10:19 +0100

    Only users assigned issue can see the change issue dropdown

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,8 +1,8 @@
 "use client";
 import Skeleton from "@/app/components/Skeleton";
-import { Issue, User } from "@prisma/client";
+import useUsers from "@/app/hooks/useUsers";
+import { Issue } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
@@ -46,13 +46,5 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
-
-const useUsers = () =>
-  useQuery({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, // 60 sec
-    retry: 3,
-  });
 
 export default AssigneeSelect;
--- app/issues/[id]/IssueDetailsData.tsx
+++ app/issues/[id]/IssueDetailsData.tsx
@@ -1,17 +1,23 @@
+import authOptions from "@/app/api/auth/providers/AuthOptions";
 import { IssueStatus } from "@/app/components";
 import ChangeIssueStatus from "@/app/components/ChangeIssueStatus";
 import { Issue } from "@prisma/client";
-import { Heading, Flex, Card } from "@radix-ui/themes";
-import React from "react";
+import { Card, Flex, Heading } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import Markdown from "react-markdown";
 
-const IssueDetailsData = ({ issue }: { issue: Issue }) => {
+const IssueDetailsData = async ({ issue }: { issue: Issue }) => {
+  // get the current user id
+  const session = await getServerSession(authOptions);
+  const assignedUser = session?.user?.id === issue.assignedToUserId;
+  // comapare the user id with the current assigned user
+
   return (
     <>
       <Heading>{issue.title}</Heading>
       <Flex gap="3" my="3">
         <IssueStatus status={issue.status} />
-        <ChangeIssueStatus issue={issue} />
+        {assignedUser && <ChangeIssueStatus issue={issue} />}
         <p>{issue.createdAt.toDateString()}</p>
       </Flex>
       <Card className="prose max-w-full">
