d0f7279168a05144cd8187c9bc45ecd61c5d9fc1
Nyikwagh Moses <mosnyik@gmail.com>
2025-03-04 07:01:27 +0100

    now sort does not clear the params and remains on page reload

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const statuses: { label: string; value?: Status }[] = [
     { label: "All" },
     { label: "Open", value: "OPEN" },
@@ -14,8 +15,13 @@ const IssueStatusFilter = () => {
   ];
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") ?? ""}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : "";
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const query = params.size ? `?${params.toString()}` : "";
         console.log("from issue status filter", status);
 
         router.push(`/issues/list${query}`);
