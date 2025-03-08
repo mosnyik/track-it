ac7159ecc742f09d47861025d5ccc9c0a76da02f
Nyikwagh Moses <mosnyik@gmail.com>
2025-03-08 14:27:50 +0100

    Added metadata for SEO

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -40,4 +40,16 @@ const IssueDetailsPage = async ({ params }: Props) => {
   );
 };
 
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt((await params).id),
+    },
+  });
+  return {
+    title: issue?.title,
+    description: " Description of issue " + issue?.id,
+  };
+}
+
 export default IssueDetailsPage;
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,6 +5,7 @@ import { prisma } from "@/prisma/prismaClient";
 import { Status } from "@prisma/client";
 import { Box, Flex } from "@radix-ui/themes";
 import IssuesTable, { columnNumber } from "./IssuesTable";
+import { Metadata } from "next";
 
 interface Props {
   searchParams: IssueQuery;
@@ -43,60 +44,6 @@ const Issues = async ({ searchParams }: Props) => {
         </Flex>
       ) : (
         <IssuesTable searchParams={resolvedParams} issues={issues} />
-        // <Table.Root variant="surface">
-        //   <Table.Header>
-        //     <Table.Row>
-        //       {columns.map((column) => (
-        //         <Table.ColumnHeaderCell
-        //           key={column.value}
-        //           className={column.className}
-        //         >
-        //           <NextLink
-        //             href={{
-        //               query: {
-        //                 ...resolvedParams,
-        //                 orderBy: column.value,
-        //               },
-        //             }}
-        //           >
-        //             {column.label}
-        //             {column.value === resolvedParams.orderBy && (
-        //               <ArrowUpIcon className="inline" />
-        //             )}
-        //           </NextLink>
-        //         </Table.ColumnHeaderCell>
-        //       ))}
-        //     </Table.Row>
-        //   </Table.Header>
-
-        //   <Table.Body>
-        //     {issues.map((issue) => (
-        //       <Table.Row key={issue.id}>
-        //         <Table.RowHeaderCell>
-        //           <Link href={`/issues/${issue.id}`}>
-        //             <div>
-        //               {issue.title}
-        //               {issue.assignedToUserId && (
-        //                 <AssignedUser
-        //                   assignedToUserId={issue.assignedToUserId ?? ""}
-        //                 />
-        //               )}
-        //             </div>
-        //           </Link>
-        //           <div className=" block md:hidden">
-        //             <IssueStatus status={issue.status} />
-        //           </div>
-        //         </Table.RowHeaderCell>
-        //         <Table.Cell className="hidden md:table-cell">
-        //           <IssueStatus status={issue.status} />
-        //         </Table.Cell>
-        //         <Table.Cell className="hidden md:table-cell">
-        //           {issue.createdAt.toDateString()}
-        //         </Table.Cell>
-        //       </Table.Row>
-        //     ))}
-        //   </Table.Body>
-        // </Table.Root>
       )}
       <Pagination
         itemCount={issueCount}
@@ -107,5 +54,34 @@ const Issues = async ({ searchParams }: Props) => {
   );
 };
 export const dynamic = "force-dynamic";
+export const metadata: Metadata = {
+  title: "Issues | Track It - Manage and Track Issues",
+  description:
+    "View, filter, sort, and add new issues with Track It. Keep your project on track with an organized and efficient issue management system.",
+  keywords:
+    "issue tracker, bug tracker, project management, task management, software issues, filter issues, sort issues, Track It",
+  openGraph: {
+    title: "Track It - Issue Management",
+    description:
+      "Manage software issues efficiently with Track It. Filter, sort, and create new issues to streamline your workflow.",
+    url: "https://loacalhost/issues/list",
+    type: "website",
+    images: [
+      {
+        url: "https://loacalhost/issues/list-preview.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Track It issues page showing filters and issue list",
+      },
+    ],
+  },
+  // twitter: {
+  //   card: "Track-it-issue-list-page",
+  //   title: "Track It - Issue Management",
+  //   description:
+  //     "Easily track, filter, and organize your project issues with Track It. A powerful issue tracker built for efficiency.",
+  //   images: ["https://loacalhost/issues/list-preview.jpg"],
+  // },
+};
 
 export default Issues;
